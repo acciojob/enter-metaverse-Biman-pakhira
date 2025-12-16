61907bd0444e6ca1a6d8603f1ff3c0c4306cb5dc
Biman Pakhira <bimanpakhira2@gmail.com>
2025-12-16 20:30:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let text = document.querySelector("#status");
+const btn = document.querySelector("#enterBtn");
+btn.addEventListener("click", ()=>{
+	text.innerHTML = "<h1>Entered Metaverse<h1>";
+})
